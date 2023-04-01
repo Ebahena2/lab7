5a46bb137ec6d1fdda65f0cc06c2539ae97cc9cf
Ebahena2 <89425354+Ebahena2@users.noreply.github.com>
2023-03-31 22:25:02 -0500

    updating js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
  
+var alerd = document.getElementById('entrybutton');
 
+alerd.addEventListener('click', function(){
+  alert('Edwin Bahena: This is a test');
 
 
 
