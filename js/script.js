6e61e863acdb6b34be380fcff837fa2be8596efe
Ebahena2 <89425354+Ebahena2@users.noreply.github.com>
2023-03-31 22:51:06 -0500

    updated javascript to do the required things for lab

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+function myEventFunction(){
+  var textbox = document.getElementById('entryinput');
+  document.getElementById('textoutput').innerHTML = textbox.value;
+  alert('Edwin Bahena:' + textbox.value);
+}
+
+
+
 var alerd = document.getElementById('entrybutton');
 
-alerd.addEventListener('click', function(){
-  alert('Edwin Bahena: This is a test');
+alerd.addEventListener('click', myEventFunction);
 
-
-
-
-
-
+}
 window.addEventListener('load', init);
